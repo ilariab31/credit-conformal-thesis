a1f401c27f91ddde307261828605b16ef042c24d
Ilaria <ilariaberardi@gmail.com>
2026-06-18 10:10:16 +0100

    Log leakage-verification finding in results log

--- src/01_load_data.R
+++ src/01_load_data.R
@@ -12,22 +12,3 @@ cat("Columns:", ncol(loans_raw), "\n")
 
 # What are the column names?
 names(loans_raw)
-
--------
-  
-  # Distribution of values and missingness for the two fields
-  loans_raw[, .N, by = num_tl_30dpd][order(num_tl_30dpd)]
-loans_raw[, .N, by = num_tl_120dpd_2m][order(num_tl_120dpd_2m)]
-
-# How many are missing (NA)?
-cat("num_tl_30dpd missing:", sum(is.na(loans_raw$num_tl_30dpd)), "\n")
-cat("num_tl_120dpd_2m missing:", sum(is.na(loans_raw$num_tl_120dpd_2m)), "\n")
-
-------
-  
-  # Average value of each field, split by whether the loan was charged off
-  loans_raw[, .(
-    avg_30dpd = mean(num_tl_30dpd, na.rm = TRUE),
-    avg_120dpd = mean(num_tl_120dpd_2m, na.rm = TRUE),
-    n = .N
-  ), by = loan_status][order(-n)]
